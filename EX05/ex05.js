9f1041899826272c16e3143d4a3ed16d18c2bfb8
Arthur Danielson <main.danielson@gmail.com>
2024-06-12 14:12:46 -0400

    adding ex05

---
+++ EX05/ex05.js
@@ -0,0 +1,10 @@
+/*
+5) Escreva um algoritmo para ler um valor (do teclado) e escrever (na tela) o seu antecessor. 
+
+Arthur Danielson Viana
+16/02/2024
+*/
+
+let num = prompt("Digite um número: ");
+
+document.write(num - 1)
