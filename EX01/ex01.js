701987fe0eebaf86ab3cb29c8138ef18eb281853
Arthur Danielson <main.danielson@gmail.com>
2024-06-12 14:06:51 -0400

    first commit

---
+++ EX01/ex01.js
@@ -0,0 +1,18 @@
+/*
+1) Escreva um algoritmo que armazene o valor 10 em uma variável A e o valor 20 em uma variável B.
+A seguir (utilizando apenas atribuições entre variáveis) troque os seus conteúdos fazendo com que o
+valor que está em A passe para B e vice-versa. Ao final, escrever os valores que ficaram armazenados
+nas variáveis.
+
+Arthur Danielson Viana
+12/06/2024
+*/
+
+let A = 10;
+let B = 20;
+let C = A;
+A = B;
+B = C;
+
+console.log(A)
+console.log(B)
