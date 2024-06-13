84d950b664f545d2f09df5787bf7040afc3c07c4
Arthur Danielson <main.danielson@gmail.com>
2024-06-13 11:07:19 -0400

    adding ex15

---
+++ EX15/ex15.js
@@ -0,0 +1,14 @@
+/*
+15) Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo). 
+
+Arthur Danielson Viana
+13/06/2024
+*/
+
+let num = -1;
+
+if (num >= 0) {
+  console.log("positivo");
+} else {
+  console.log("negativo");
+};
