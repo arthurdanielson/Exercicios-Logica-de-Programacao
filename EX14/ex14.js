1e24d5bdc8b9b4a4ce56df8f96a1dc4c14cce96e
Arthur Danielson <main.danielson@gmail.com>
2024-06-13 11:05:10 -0400

    adding ex14

---
+++ EX14/ex14.js
@@ -0,0 +1,15 @@
+/*
+14) Ler um valor e escrever a mensagem É MAIOR QUE 10! se o valor lido for maior que 10, caso
+contrário escrever NÃO É MAIOR QUE 10! 
+
+Arthur Danielson Viana
+13/06/2024
+*/
+
+let num = 11;
+
+if (num >= 10) {
+  console.log("É MAIOR QUE 10!");
+} else {
+  console.log("NÃO É MAIOR QUE 10!");
+};
