a4c0712377ad1be9b4b041e101e7520ad35469df
Arthur Danielson <main.danielson@gmail.com>
2024-06-13 11:21:06 -0400

    adding ex19

---
+++ EX19/ex19.js
@@ -0,0 +1,16 @@
+/*
+19) Ler dois valores (considere que não serão lidos valores iguais) e escrever o maior deles. 
+
+Arthur Danielson Viana
+13/06/2024
+*/
+
+let num1 = 12;
+let num2 = 15;
+
+if (num1 > num2) {
+  console.log(num1);
+} else {
+  console.log(num2);
+};
+
