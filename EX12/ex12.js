64b7cccdbd003d9b2fab3045744fc84ebdbd8ee3
Arthur Danielson <main.danielson@gmail.com>
2024-06-13 00:36:51 -0400

    adding ex12

---
+++ EX12/ex12.js
@@ -0,0 +1,17 @@
+/*
+12) Escreva um algoritmo para ler uma temperatura em graus Fahrenheit, calcular e escrever o valor
+correspondente em graus Celsius (baseado na fórmula abaixo):
+    C          F - 32
+---------- = -----------
+    5             9
+Observação: Para testar se a sua resposta está correta saiba que 100oC = 212F 
+
+Arthur Danielson Viana
+13/06/2024
+*/
+
+let celsius = 100;
+
+let fahrenheit = (9 * celsius + 160) / 5;
+
+console.log(fahrenheit);
