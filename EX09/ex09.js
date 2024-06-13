812c1df6bbfd3587d3ca066ced3a3881a398be80
Arthur Danielson <main.danielson@gmail.com>
2024-06-13 00:15:17 -0400

    adding ex09

---
+++ EX09/ex09.js
@@ -0,0 +1,13 @@
+/**
+ * 9) Escreva um algoritmo para ler o salário mensal atual de um funcionário e o percentual de reajuste. Calcular e escrever o valor do novo salário.
+ * 
+ * Arthur Danielson Viana
+ * 13/06/2024
+ */
+
+let salario = 100;
+let reajuste = 10;
+
+let novoSalario = salario + (reajuste / salario * 100);
+
+console.log(novoSalario)
