4dec20fa53d5b660e6fb9c4fef63b14851cb806f
Arthur Danielson <main.danielson@gmail.com>
2024-06-13 11:22:52 -0400

    adding ex20

---
+++ EX20/ex20.js
@@ -0,0 +1,15 @@
+/*
+20) Ler dois valores (considere que não serão lidos valores iguais) e escrevê-los em ordem crescente. 
+
+Arthur Danielson Viana
+13/06/2024
+*/
+
+let num1 = 15;
+let num2 = 12;
+
+if (num1 > num2) {
+  console.log(num2, num1);
+} else {
+  console.log(num1, num2);
+};
