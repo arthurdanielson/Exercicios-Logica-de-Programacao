e25e600ac2ae4fcd41a492c4abacf67a2807ad37
Arthur Danielson <main.danielson@gmail.com>
2024-06-14 00:21:50 -0400

    adding ex28

---
+++ EX28/ex28.js
@@ -0,0 +1,18 @@
+/*
+28) Ler 3 valores (considere que não serão informados valores iguais) e escrever o maior deles. 
+
+Arthur Danielson Viana
+14/06/2024
+*/
+
+let n1 = 3
+let n2 = 2
+let n3 = 4
+
+if (n1 > n2 && n1 > n3) {
+  console.log(n1)
+} else if (n2 > n1 && n2 > n3) {
+  console.log(n2)
+} else {
+  console.log(n3)
+}
