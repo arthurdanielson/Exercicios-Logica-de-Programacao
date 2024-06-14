1380f53d7dbf060bdb3a290d6cf500796a5820e1
Arthur Danielson <main.danielson@gmail.com>
2024-06-14 00:49:44 -0400

    adding ex33

---
+++ EX33/ex33.js
@@ -0,0 +1,20 @@
+/*
+33) Ler dois valores e imprimir uma das três mensagens a seguir:
+‘Números iguais’, caso os números sejam iguais
+‘Primeiro é maior’, caso o primeiro seja maior que o segundo;
+‘Segundo maior’, caso o segundo seja maior que o primeiro. 
+
+Aluno: Arthur Danielson Viana
+Data: 14/06/2024
+*/
+
+let n1 = 1
+let n2 = 1
+
+if (n1 > n2) {
+  console.log("Primeiro é maior")
+} else if (n2 > n1) {
+  console.log("Segundo maior")
+} else {
+  console.log("Números iguais")
+}
