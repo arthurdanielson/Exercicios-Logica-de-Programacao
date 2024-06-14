156e4c9021175a3cdb7060238947f80c8f071e06
Arthur Danielson <main.danielson@gmail.com>
2024-06-14 00:19:13 -0400

    adding ex27

---
+++ EX27/ex27.js
@@ -0,0 +1,16 @@
+/*
+27) Ler um valor e escrever se é positivo, negativo ou zero.
+
+Arthur Danielson Viana
+14/06/2024
+*/
+
+let num = 0
+
+if (num > 0) {
+  console.log("positivo")
+} else if (num < 0) {
+  console.log("negativo")
+} else {
+  console.log("zero")
+}
