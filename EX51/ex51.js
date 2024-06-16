f2dc74157cc9b7ee61f8de5ab23de202de3dbd42
Arthur Danielson <main.danielson@gmail.com>
2024-06-15 23:59:43 -0400

    adding ex51

---
+++ EX51/ex51.js
@@ -0,0 +1,11 @@
+/*
+51)  Escreva  um  algoritmo  para  imprimir  os  números  de  1  (inclusive)  a  10  (inclusive)  em  ordem 
+decrescente.
+
+Aluno: Arthur Danielson Viana
+Data: 15/06/2024
+*/
+
+for (let i = 10; i >= 1; i--) {
+  console.log(i)
+}
