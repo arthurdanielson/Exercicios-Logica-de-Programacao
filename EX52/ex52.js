74b7d30148f855ecad54e4ba230504923d5a375f
Arthur Danielson <main.danielson@gmail.com>
2024-06-16 00:02:14 -0400

    adding ex52

---
+++ EX52/ex52.js
@@ -0,0 +1,10 @@
+/*
+52) Escreva um algoritmo para imprimir os 10 primeiros números inteiros maiores que 100.
+
+Aluno: Arthur Danielson Viana
+Data: 16/06/2024
+*/
+
+for (let i = 101; i <= 110; i++) {
+  console.log(i)
+}
