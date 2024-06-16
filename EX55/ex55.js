ec432ebfcc93e5054f3bbed813074906965a56aa
Arthur Danielson <main.danielson@gmail.com>
2024-06-16 00:08:59 -0400

    adding ex55

---
+++ EX55/ex55.js
@@ -0,0 +1,10 @@
+/*
+55) Escreva um algoritmo que calcule e imprima a tabuada do 8 (1 a 10).
+
+Aluno: Arthur Danielson Viana
+Data: 16/06/2024
+*/
+
+for (let i = 1; i <= 10; i++) {
+  console.log(`8 x ${i} = ${8 * i}`)
+}
