c76124c5253bccf40fe959b654b2b5597df35634
Arthur Danielson <main.danielson@gmail.com>
2024-06-15 23:58:13 -0400

    adding ex50

---
+++ EX50/ex50.js
@@ -0,0 +1,11 @@
+/*
+50)  Escreva  um  algoritmo  para  imprimir  os  números  de  1  (inclusive)  a  10  (inclusive)  em  ordem 
+crescente.
+
+Aluno: Arthur Danielson Viana
+Data: 15/06/2024
+*/
+
+for (let i = 1; i <= 10; i++) {
+  console.log(i)
+}
