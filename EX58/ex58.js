e93ab63e3df953b86b77bd50d209dc42bf10444f
Arthur Danielson <main.danielson@gmail.com>
2024-06-16 14:18:15 -0400

    adding ex58

---
+++ EX58/ex58.js
@@ -0,0 +1,13 @@
+/*
+58) Reescreva o exercício 51 utilizando a estrutura ENQUANTO e um CONTADOR. 
+
+Aluno: Arthur Danielson Viana
+Data: 16/06/2024
+*/
+
+let contador = 10;
+
+do {
+  console.log(contador)
+  contador--
+} while (contador >= 1)
