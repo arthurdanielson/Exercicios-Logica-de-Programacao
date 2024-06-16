168b1d8dd1e6d06b4dde72d9890456cc3840066d
Arthur Danielson <main.danielson@gmail.com>
2024-06-16 14:16:50 -0400

    adding ex57

---
+++ EX57/ex57.js
@@ -0,0 +1,13 @@
+/*
+57) Reescreva o exercício 50 utilizando a estrutura REPITA e um CONTADOR. 
+
+Aluno: Arthur Danielson Viana
+Data: 16/06/2024
+*/
+
+let contador = 1
+
+do {
+  console.log(contador)
+  contador++
+} while (contador <= 10)
