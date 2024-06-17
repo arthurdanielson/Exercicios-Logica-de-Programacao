75ffc67c34b44c38098d99b5bf382a4164f490af
Arthur Danielson <main.danielson@gmail.com>
2024-06-17 19:18:05 -0400

    adding ex74

---
+++ EX74/ex74.js
@@ -0,0 +1,13 @@
+/*
+74) Escreva um algoritmo que imprima a tabuada (de 1 a 10) para os números de 1 a 10. 
+
+Aluno: Arthur Danielson Viana
+Data: 17/06/2024
+*/
+
+for (let i = 1; i <= 10; i++) {
+  for (let j = 1; j <= 10; j++) {
+    console.log(`${i} x ${j} = ${i * j}`)
+  }
+  console.log("")
+}
