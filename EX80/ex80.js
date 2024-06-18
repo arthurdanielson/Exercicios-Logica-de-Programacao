9df1e0bc4798ce8952b77cd6cad6fc9ce022b3c7
Arthur Danielson <main.danielson@gmail.com>
2024-06-17 23:30:30 -0400

    adding ex80

---
+++ EX80/ex80.js
@@ -0,0 +1,22 @@
+/*
+80) Ler um vetor Q de 20 posições (aceitar somente números positivos). Escrever a seguir o valor do
+maior elemento de Q e a respectiva posição que ele ocupa no vetor. 
+
+Aluno: Arthur Danielson Viana
+Data: 17/06/2024
+*/
+
+let Q = []
+for (let i = 0; i < 20; i++) {
+  let num = Number(prompt("Digite um número positivo:"))
+  if (num > 0) {
+    Q.push(num)
+  } else {
+    i--
+    console.log("Número inválido, digite novamente.")
+  }
+}
+let maior = Math.max(...Q)
+let posicao = Q.indexOf(maior)
+console.log("Maior elemento:", maior)
+console.log("Posição do maior elemento:", posicao)
